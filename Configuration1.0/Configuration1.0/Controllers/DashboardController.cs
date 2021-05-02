3f77073939845140490e05257e69660c6a7ec8dd
hridoyhr <hridoyswe205@gmail.com>
2021-05-02 16:08:29 +0600

    UI desing Project setup

---
+++ Configuration1.0/Configuration1.0/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Configuration1._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Configuration1._0.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Summary()
+        {
+            var model = new SummaryModel();
+            return View(model);
+        }
+    }
+}
