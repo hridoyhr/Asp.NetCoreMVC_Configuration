fcf12ddaee63497d705325d074d9f610d1fa8760
hridoyhr <hridoyswe205@gmail.com>
2021-05-03 12:01:10 +0600

    Update MVC Configuration

---
+++ ConfigDemo5.0/ConfigDemo5.0/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using ConfigDemo5._0.Models.Special;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigDemo5._0.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            var model = new AccountModel();
+            return View(model);
+        }
+    }
+}
---
+++ ConfigDemo5.0/ConfigDemo5.0/Models/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigDemo5._0.Models.Special
+{
+    public class AccountModel
+    {
+        public string Name;
+
+        public AccountModel()
+        {
+            Name = "Mahmudul ";
+        }
+    }
+}
