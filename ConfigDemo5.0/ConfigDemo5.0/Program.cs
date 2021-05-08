4efe177d5e68da5c2a3443b0e1853f4942492661
hridoyhr <hridoyswe205@gmail.com>
2021-05-08 22:57:58 +0600

    In Program.cs Active Autofac and install Autofac.Extensions.DependencyInjection nugget package

--- ConfigDemo5.0/ConfigDemo5.0/Program.cs
+++ ConfigDemo5.0/ConfigDemo5.0/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -45,6 +46,8 @@ namespace ConfigDemo5._0
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new 
+                AutofacServiceProviderFactory())
                 .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
